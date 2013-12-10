2a49d8b1ee14bb8687a37731f520c4d2bff633d6
Russell Jurney <russell.jurney@gmail.com>
2013-12-09 17:08:09 -0800

    Have chart data for time of day working.

--- index.py
+++ index.py
@@ -15,12 +15,32 @@ users = db['users']
 reviews = db['reviews']
 checkins = db['checkins']
 
+def init_hash():
+    h = {}
+    for i in range(0,23):
+        h[str(i)] = 0
+    return h
+
+def process_hours(hours_days):
+    hour_chart_data = init_hash()
+    for hour_day in hours_days:
+        hour, day = parse_hour(hour_day)
+        print hours_days[hour_day]
+        hour_chart_data[str(hour)] += hours_days[hour_day]
+    return hour_chart_data
+
+def parse_hour(hour_day):
+    return hour_day.split("-")
+
 # Controller: Fetch a business and display it
 @app.route("/business/<business_id>")
 def business(business_id):
   business = businesses.find_one({'business_id': business_id})
   print "Business: " + str(business)
-  return render_template('partials/business.html', business=business)
+  checks = checkins.find_one({'business_id': business_id})
+  hours = process_hours(checks['checkin_info'])
+  hours_json = json.dumps(hours)
+  return render_template('partials/business.html', business=business, hours_json=hours_json, hours=hours)
 
 if __name__ == "__main__":
   app.run(debug=True)
