d1188c9fbb5d12f9ef3b3aec5fd3d4bd69a97d28
Russell Jurney <russell.jurney@gmail.com>
2013-12-09 17:26:57 -0800

    Have a sorted list of hours working ok for a data table

--- index.py
+++ index.py
@@ -18,7 +18,7 @@ checkins = db['checkins']
 def init_hash():
     h = {}
     for i in range(0,23):
-        h[str(i)] = 0
+        h[i] = 0
     return h
 
 def process_hours(hours_days):
@@ -26,7 +26,7 @@ def process_hours(hours_days):
     for hour_day in hours_days:
         hour, day = parse_hour(hour_day)
         print hours_days[hour_day]
-        hour_chart_data[str(hour)] += hours_days[hour_day]
+        hour_chart_data[int(hour)] += hours_days[hour_day]
     return hour_chart_data
 
 def parse_hour(hour_day):
