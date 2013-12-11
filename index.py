ccd54c4a4e651749022932475f258bf9b14b3c9f
Russell Jurney <russell.jurney@gmail.com>
2013-12-11 14:56:56 -0800

    Bar chart of checkin hours working.

--- index.py
+++ index.py
@@ -15,22 +15,21 @@ users = db['users']
 reviews = db['reviews']
 checkins = db['checkins']
 
-def init_hash():
-    h = {}
-    for i in range(0,23):
-        h[i] = 0
+def init_zero_hash():
+    h = []
+    for i in range(0,24):
+        h.append({'label': str(i), 'value': 0})
     return h
-
-def process_hours(hours_days):
-    hour_chart_data = init_hash()
-    for hour_day in hours_days:
-        hour, day = parse_hour(hour_day)
-        print hours_days[hour_day]
-        hour_chart_data[int(hour)] += hours_days[hour_day]
-    return hour_chart_data
 
 def parse_hour(hour_day):
     return hour_day.split("-")
+
+def process_hours(hours_days):
+    hour_chart_data = init_zero_hash()
+    for hour_day in hours_days:
+        hour, day = parse_hour(hour_day)
+        hour_chart_data[int(hour)]['value'] += hours_days[hour_day]
+    return hour_chart_data
 
 # Controller: Fetch a business and display it
 @app.route("/business/<business_id>")
@@ -39,7 +38,7 @@ def business(business_id):
   print "Business: " + str(business)
   checks = checkins.find_one({'business_id': business_id})
   hours = process_hours(checks['checkin_info'])
-  hours_json = json.dumps(hours)
+  hours_json = json.dumps([{'key': 'Checkins Per Hour', 'values': hours}])
   return render_template('partials/business.html', business=business, hours_json=hours_json, hours=hours)
 
 if __name__ == "__main__":
