ea3df15249203ce516339af59fa79c0cd7edd3b9
Russell Jurney <russell.jurney@gmail.com>
2013-12-23 21:35:06 -0500

    noop on mongo indexes

--- mongo_indexes.js
+++ mongo_indexes.js
@@ -3,5 +3,5 @@ db.checkins.ensureIndex({'business_id': 1});
 db.nearest_businesses.ensureIndex({'business_id': 1});
 db.reviews.ensureIndex({'review_id': 1});
 db.reviews.ensureIndex({'business_id': 1});
-db.ntf_idf_adjectives_per_business.ensureIndex({'business_id': 1});
+db.ntf_idf_words_per_business.ensureIndex({'business_id': 1});
 db.raw_words_per_business.ensureIndex({'business_id': 1});
