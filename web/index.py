39b995758c520b76df4349495033af5fdb6c80ed
Russell Jurney <russell.jurney@gmail.com>
2013-12-24 22:06:22 -0500

    App work for word cloud. Need design work.

--- web/index.py
+++ web/index.py
@@ -67,12 +67,16 @@ def business(business_id):
   revs = reviews.find({'business_id': business_id}).sort('date', pymongo.DESCENDING)
   nearby = nearest_businesses.find_one({'business_id': business_id})
   zoom_level = map_degree_to_zoom(nearby['range'])
+  words_per_business = db['ntf_idf_words_per_business'].find_one({'business_id': business_id})
+  if words_per_business:
+    words_per_business = json.dumps(words_per_business['ntf_idf_scores'])
   return render_template('partials/business.html', business=business, 
                                                    hours_json=hours_json, 
                                                    hours=hours, 
                                                    revs=revs, 
                                                    nearby=nearby['nearest_businesses'], 
-                                                   zoom_level=zoom_level)
+                                                   zoom_level=zoom_level,
+                                                   words_per_business=words_per_business)
 
 # Controller: Fetch a review and display it
 @app.route("/review/<review_id>")
